6290cc94c39c5ed7c1a82dc2c090d1cd481d149e
Ivan Kochin <iv.kochin@gmail.com>
2020-06-23 15:09:58 +0300

    Update 01_templates/01_08_misc/01_08_01_template_aliases.cpp

--- 01_templates/01_08_misc/01_08_01_template_aliases.cpp
+++ 01_templates/01_08_misc/01_08_01_template_aliases.cpp
@@ -3,9 +3,9 @@
 template <size_t Size = 10> 
 using integral_array = std::array<int, Size>;
 
-
+template<typename T>
 struct integral_vector {
-    using iterator = std::size_t*;
+    using iterator = T*;
     iterator begin;
     iterator end;
 };
@@ -14,5 +14,5 @@ int main() {
     integral_array<> my_arr1;
     integral_array<15> my_arr2;
 
-    integral_vector::iterator it;
+    integral_vector<int>::iterator it;
 }
