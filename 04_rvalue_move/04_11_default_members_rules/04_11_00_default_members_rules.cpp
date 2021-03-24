4de98fe7a99d03d7080655e47bad462064263c06
Kochin, Ivan <kochin.ivan@intel.com>
2021-03-24 17:45:53 +0300

    Minor move semantic lecture improvements

--- 04_rvalue_move/04_11_default_members_rules/04_11_00_default_members_rules.cpp
+++ 04_rvalue_move/04_11_default_members_rules/04_11_00_default_members_rules.cpp
@@ -1,19 +1,14 @@
 // Show the difference between deleted and not defined move CTOR.
 
-#include <iostream>
+#include <utility>
 
 struct A {
     A() = default;
     A(const A&) = default;
-//    A(A&&) = delete;
+    A(A&&) = delete;
 };
-
-A foo() {
-    A a;
-    return a;
-}
 
 int main() {
     A a1;
-    A a2(foo());
+    A a2 = std::move(a1);
 }
