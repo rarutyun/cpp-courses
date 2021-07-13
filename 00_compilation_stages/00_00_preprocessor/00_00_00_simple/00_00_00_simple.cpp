0e1ddac988fd83e5bf66ed92e87827a6465cf441
Elizarova, Alina <alina.elizarova@intel.com>
2021-07-13 21:55:17 +0300

    Update compilation stages: move description from slides to source code

--- 00_compilation_stages/00_00_preprocessor/00_00_00_simple/00_00_00_simple.cpp
+++ 00_compilation_stages/00_00_preprocessor/00_00_00_simple/00_00_00_simple.cpp
@@ -1,4 +1,16 @@
-// Show work of preprocessor(use -E)
+// Preprocessor behavior:
+// * Removes comments
+// * Replaces #define macros with their values
+// * Selects the necessary pieces of code according to the conditions of #if,
+//   #ifdef and #ifndef
+// * Includes header files content
+//
+// Command example to run only preprocessor:
+// g++ -E 00_00_00_simple.cpp -o 00_00_00_simple.ii
+//
+// * -E runs preprocessor for a given file without compilation
+// * -o sets the name of the resulting file (extension name (.ii) is arbitrary)
+
 // What is removed after preprocessing?
 // What does remain after preprocessing?
 
--- 00_compilation_stages/00_01_compilation/00_01_00_compilation_error/00_01_00_compilation_error.cpp
+++ 00_compilation_stages/00_01_compilation/00_01_00_compilation_error/00_01_00_compilation_error.cpp
@@ -1,6 +1,7 @@
 // What is a compilation error?
 // Сompile and show compilation error
 // Fix error(order is important)
+// Show assembler code (use -S)
 
 #include "00_01_00_compilation_error.h"
 int a = 5;
